699d83d7859b4aad91d4ae5d293c0ed7095e284d
Franck Boucher <boucher.fk@gmail.com>
2024-04-22 23:14:27 +0200

    Add clerk + getAuth on loader and action

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
+import type { LoaderFunction } from "@vercel/remix";
+
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp } from "@clerk/remix";
+
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -26,6 +32,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
+function App() {
   return <Outlet />;
 }
+
+export default ClerkApp(App);
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,12 @@
-import type { MetaFunction } from "@vercel/remix";
+import type { ActionFunction, LoaderFunction, MetaFunction } from "@vercel/remix";
+import {
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  UserButton,
+} from "@clerk/remix";
+import { getAuth } from "@clerk/remix/ssr.server";
+import { Form } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +15,34 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader: LoaderFunction = async (args) => {
+  const { userId } = await getAuth(args);
+  console.log('loader', userId);
+  return {};
+}
+
+export const action: ActionFunction = async (args) => {
+  const { userId } = await getAuth(args);
+  console.log('action', userId);
+  return {};
+}
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <SignedIn>
+        <h1>Index route</h1>
+        <p>You are signed in!</p>
+
+        <Form method='post'>
+          <button type='submit'>Submit</button>
+        </Form>
+
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </div>
   );
 }
